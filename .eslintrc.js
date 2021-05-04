0b0dbfe4a45b8c35540a652ea78e3462400b7be2
brteo <51654355+brteo@users.noreply.github.com>
2021-05-04 12:34:45 +0200

    setup CRA + eslint/prittier

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+	parserOptions: {
+		ecmaVersion: 2020, // Use the latest ecmascript standard
+		sourceType: 'module', // Allows using import/export statements
+		ecmaFeatures: {
+			jsx: true // Enable JSX since we're using React
+		}
+	},
+	settings: {
+		react: {
+			version: 'detect' // Automatically detect the react version
+		}
+	},
+	env: {
+		browser: true, // Enables browser globals like window and document
+		amd: true, // Enables require() and define() as global variables as per the amd spec.
+		node: true, // Enables Node.js global variables and Node.js scoping.
+		jest: true // Enable Jest test
+	},
+	plugins: ['react', 'prettier', 'react-hooks'],
+	extends: [
+		'eslint:recommended',
+		'airbnb',
+		'plugin:react/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:prettier/recommended'
+	],
+	rules: {
+		'prettier/prettier': ['error'],
+		'react/react-in-jsx-scope': 'off', // suppress errors for missing 'import React' in files
+		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], // allow jsx syntax in js files
+		'max-len': ['error', { code: 120, ignoreComments: true }]
+	}
+};
+
+/*
+* OLD RULES
+		'prefer-promise-reject-errors': ['off'],
+		'no-return-assign': ['off'],
+		'no-console': ['off'],
+		indent: ['error', 'tab', { SwitchCase: 1 }],
+		'no-unused-vars': ['warn', { vars: 'all', args: 'none', ignoreRestSiblings: true }],
+		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+		'prefer-template': ['off'],
+		'no-nested-ternary': ['off'],
+		'no-plusplus': ['off'],
+		'react/prop-types': ['warn'],
+		'react/prefer-stateless-function': ['off'],
+		'react/jsx-indent': ['error', 'tab'],
+		'react/jsx-indent-props': ['error', 'tab'],
+		'react/prop-types': ['off'],
+		'react/destructuring-assignment': ['off'],
+		'react/display-name': ['off'],
+		'react/jsx-props-no-spreading': ['off'],
+		'react-hooks/exhaustive-deps': 'warn'
+
+*/
--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # assignment2-2
+
 Programmazione Concorrente e Distribuita - Assignment 2-2
+
+## Create-react-app
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+It sets up your development environment so that you can use the latest JavaScript features, provides a nice developer experience, and optimizes your app for production. You’ll need to have Node >= 10.16 and npm >= 5.6 on your machine.
+
+Create React App doesn’t handle backend logic or databases; it just creates a frontend build pipeline, so you can use it with any backend you want. Under the hood, it uses Babel and webpack, but you don’t need to know anything about them.
+
+```sh
+npm install -g create-react-app  # global install of CRA
+npm init react-app . --legacy-peer-deps # create CRA
+	# add flag --legacy-peer-deps with npm version > 7.0
+```
+
+### Commands
+
+```sh
+npm start
+```
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+```sh
+npm test
+```
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+```sh
+npm run build
+```
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+## Packages
+
+- (DEV) eslint eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks
+- (DEV) prettier eslint-config-prettier eslint-plugin-prettier
